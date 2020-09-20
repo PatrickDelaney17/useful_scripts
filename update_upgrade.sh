676aaf04be5c229669582c93fddb718a9c80139f
PatrickDelaney17 <Delaney.Patrick17@gmail.com>
2020-09-20 13:12:43 -0400

    Delete update_upgrade.sh

--- update_upgrade.sh
+++
@@ -1,9 +0,0 @@
-echo "checking for updates"
-
-sudo apt-get update -y
-
-echo -e "update command --> finished \n~~~~~~~~~~~~~~~~~~~~~ "
-
-echo "checking for upgrade --> follow with reboot either way..."
-
-sudo apt-get dist-upgrade -y && sudo shutdown -r
