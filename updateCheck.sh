730d11eae323f8666fd0b15d4e953b4c8c39073f
Patrick Delaney <delaney.patrick17@gmail.com>
2021-05-23 13:24:26 -0400

    clean up / consolidate

--- updateCheck.sh
+++ updateCheck.sh
@@ -1,59 +1,56 @@
 #!/bin/bash
 
-
 GREEN="\033[1;32m"
 NOCOLOR="\033[0m"
 
 #REF - https://codereview.stackexchange.com/questions/146896/simple-linux-upgrade-script-in-bash
 #REF - https://www.raspberrypi.org/documentation/raspbian/updating.md
 
-basic_msg(){
-   for i in "$*"; do echo "$i"; done;
+basic_msg() {
+	for i in "$*"; do echo "$i"; done
 }
 
-green_msg(){
- for i in "$*"; do echo -e "${GREEN} $i ${NOCOLOR}"; done;
+green_msg() {
+	for i in "$*"; do echo -e "${GREEN} $i ${NOCOLOR}"; done
 }
-red_msg()
-{
-RED="\033[1;31m"
-for i in "$*"; do echo -e "${RED} $i ${NOCOLOR}"; done;
+red_msg() {
+	RED="\033[1;31m"
+	for i in "$*"; do echo -e "${RED} $i ${NOCOLOR}"; done
 }
 
-disk_spc(){
+disk_spc() {
 	# Total available
-	AVAIL=`df -k --total --output=avail "$PWD" | tail -n1`
+	AVAIL=$(df -k --total --output=avail "$PWD" | tail -n1)
 	#1 GB in block size
 	GB=2097152
 	#If we have more than a gb continue
-	if [[ $AVAIL -lt $GB ]]
-		then 
+	if [[ $AVAIL -lt $GB ]]; then
 		red_msg "|0_0| Killing the script not enough space on disk"
 		exit 130
-		else		
+	else
 		green_msg "^_^ Disk space looks good no action needed!"
 		echo
-		fi
+	fi
 }
 
-check_pihole(){
+check_pihole() {
 
 	echo
 	STR=$(pihole -up --check-only)
 	SUB='Everything is up to date'
 
 	green_msg "step 8:Check pihole for updates"
-	if [[ "$STR" == *"$SUB"* ]]; then
-	echo
-	basic_msg "No Pihole updates needed"
+	if [[ "$STR" ==*"$SUB"*]]; then
+		echo
+		basic_msg "No Pihole updates needed"
 	else
-	echo
-	red_msg "Updates available for install"
+		echo
+		red_msg "Updates available for install"
 
-	sudo pihole -up -y
-	echo
-	green_msg "Update Gravity and flush query log in Pihole"
-	sudo pihole -g -f
+		sudo pihole -up -y
+		echo
+		green_msg "Update Gravity and flush query log in Pihole"
+		sudo pihole -g -f
 	fi
 }
 
@@ -71,23 +68,13 @@ disk_spc
 
 echo
 
-green_msg "step 1: update apt cache"
-sudo apt-get update -y
+green_msg "step 1: update apt cache && step 2: upgrade packages"
+sudo apt-get update -y && sudo apt-get full-upgrade -y
 
 echo
 
-green_msg "step 2: upgrade packages"
-sudo apt-get full-upgrade -y
-
-echo
-
-green_msg "step 3: distribution upgrade"
-sudo apt-get dist-upgrade -y
-
-echo
-
-green_msg "step 4: remove unused packages"
-sudo apt-get --purge autoremove -y
+green_msg "step 3: distribution upgrade && step 4: remove unused packages"
+sudo apt-get dist-upgrade -y && sudo apt-get --purge autoremove -y
 
 echo
 
@@ -99,13 +86,12 @@ sudo apt-get autoclean
 basic_msg "check if pihole exist on system"
 echo
 
-if [[ -d "/etc/pihole" ]]
-then
-    basic_msg "Pihole found...proceed with update check"
+if [[ -d "/etc/pihole" ]]; then
+	basic_msg "Pihole found...proceed with update check"
 	check_pihole
-	else
+else
 	basic_msg "Pihole not found on system, moving on"
-	
+
 fi
 
 #TO Write comment in a file uncomment line below
@@ -119,7 +105,7 @@ echo
 df -h --total /root /dev
 echo
 basic_msg "-----------------------------"
-echo 
+echo
 echo
 basic_msg "step 6: Rebooting Pi Server"
 sudo shutdown -r
